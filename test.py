70d87862e6f2148d7c0c47d3aa81f0cdc7ee5cc5
Arun Mittal <arunmittal53@gmail.com>
2019-03-27 13:05:34 +0530

    change files names

--- test.py
+++ test.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 
-from datetime import datetime
-
-ExpectedDate = "27/3/2019 9:57"
-ExpectedDate = datetime.strptime(ExpectedDate, "%d/%m/%Y %H:%M")
-print(ExpectedDate)
-while(ExpectedDate > datetime.now()):
-    print(datetime.now())
-    print("wait")
-    sleep(1)
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
+driver = webdriver.Chrome(executable_path="./chromedriver")
+
+
+# Open up a Firefox browser and navigate to web page.
+driver.get("http://econpy.pythonanywhere.com/ex/001.html")
+
+# Extract lists of "buyers" and "prices" based on xpath.
+buyers = driver.find_elements_by_xpath('//div[@title="buyer-name"]')
+prices = driver.find_elements_by_xpath('//span[@class="item-price"]')
+
+# Print out all of the buyers and prices on page:
+num_page_items = len(buyers)
+for i in range(num_page_items):
+    print(buyers[i].text + " : " + prices[i].text)
+
+# Clean up (close browser once completed task).
+driver.close()
 
 
